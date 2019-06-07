31bcb1bbdd6b9c6aa56fd4cb57478d1736199d37
Camille Dejoye <cdejoye@houra.fr>
2019-06-07 17:06:57 +0200

    ListInput handles multi selection mode
    Adds a parameter to ListInput so it can let the use know that multiple
    selection is possible.
    By default multiple selection is off to not change its behaviour.

--- lib/Response/Input/ListInput.php
+++ lib/Response/Input/ListInput.php
@@ -4,8 +4,24 @@ namespace Phpactor\Extension\Rpc\Response\Input;
 
 class ListInput extends ChoiceInput
 {
+    private $allowMultipleResults = false;
+
     public function type(): string
     {
         return 'list';
+    }
+
+    public function setAllowMultipleResults(bool $allowMultipleResults): self
+    {
+        $this->allowMultipleResults = $allowMultipleResults;
+
+        return $this;
+    }
+
+    public function parameters(): array
+    {
+        return array_merge(parent::parameters(), [
+            'multi' => $this->allowMultipleResults,
+        ]);
     }
 }
